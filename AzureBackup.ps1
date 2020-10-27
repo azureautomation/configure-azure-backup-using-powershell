e1fcecfc58a2756b1125e1ecd559325916fd349f
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 13:58:10 +0530

    initial commit

---
+++ AzureBackup.ps1
@@ -0,0 +1,41 @@
+﻿
+#Variables for common values
+
+$NameResourceGroup = "AAA-Production"
+$NameRecoveryServicesVault = "A123RecoveryServicesVault"
+$location = "WestUS2"
+$NamePolocy = "DefaultPolicy"
+$vmName = "AAA-Prod-SVR1"
+$FriendlyName = "AAAProdSVR1"
+
+#Create a recovery services vaults
+
+New-AzureRmRecoveryServicesVault `
+    -ResourceGroupName $NameResourceGroup `
+    -Name $NameResourceGroup `
+    -Location $location
+
+#Create a recovery services vaults
+
+Get-AzureRmRecoveryServicesVault `
+    -Name $NameRecoveryServicesVault | Set-AzureRmRecoveryServicesVaultContext
+
+# Enable backup for an Azure VM
+
+$policy = Get-AzureRmRecoveryServicesBackupProtectionPolicy -Name $NamePolocy
+Enable-AzureRmRecoveryServicesBackupProtection `
+    -ResourceGroupName $NameResourceGroup `
+    -Name $vmName `
+    -Policy $policy
+
+# Start a backup job
+
+$backupcontainer = Get-AzureRmRecoveryServicesBackupContainer `
+    -ContainerType "AzureVM" `
+    -FriendlyName $FriendlyName
+
+$item = Get-AzureRmRecoveryServicesBackupItem -Container $backupcontainer
+    -WorkloadType "AzureVM"
+    
+Backup-AzureRmRecoveryServicesBackupItem -Item $item
+
